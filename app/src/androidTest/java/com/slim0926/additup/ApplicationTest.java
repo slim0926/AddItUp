ba6cbe693b4681ecc3bff77a7a9041ccd2d94661
sue lim <usmile0926@gmail.com>
2016-07-12 14:59:38 -0400

    Added new files

---
+++ app/src/androidTest/java/com/slim0926/additup/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.slim0926.additup;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
